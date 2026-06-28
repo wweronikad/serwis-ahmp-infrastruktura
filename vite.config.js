b4ff19a5a3395d8f130f93e60db2a134f530c4e7
wweronikad <weronikadz1405@gmail.com>
2026-06-28 16:59:03 +0200

    Set base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 export default defineConfig({
   plugins: [react()],
   root: __dirname,
-  base: process.env.NODE_ENV === 'production' ? '/ahmp-serwis/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/serwis-ahmp-infrastruktura/' : '/',
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
     strictPort: false,
